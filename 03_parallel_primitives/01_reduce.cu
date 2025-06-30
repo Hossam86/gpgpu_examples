cf6d04df445ffd51ea59d4d1e60f4d927c825498
Hossam Ali <hossamsamir@outlook.com>
2025-06-30 16:25:50 +0300

    refactoring

--- 03_parallel_primitives/01_reduce.cu
+++ 03_parallel_primitives/01_reduce.cu
@@ -1,17 +1,8 @@
+#include <cuda_runtime.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-// reduce serial implementation
-void reduce(float *dout, float *din, int n) {
-
-  float sum = 0;
-  for (int i = 0; i < n; ++i) {
-    sum += din[i];
-  }
-  *dout = sum;
-}
-
-// reduce on gpu (global memory)
-__global__ void globalMem_reduce_kernel(float *d_out, float *d_in) {
+__global__ void global_reduce_kernel(float *d_out, float *d_in) {
   int myId = threadIdx.x + blockDim.x * blockIdx.x;
   int tid = threadIdx.x;
 
@@ -29,8 +20,7 @@ __global__ void globalMem_reduce_kernel(float *d_out, float *d_in) {
   }
 }
 
-__global__ void sharedMem_reduce_kernel(float *d_out, const float *d_in) {
-
+__global__ void shmem_reduce_kernel(float *d_out, const float *d_in) {
   // sdata is allocated in the kernel call: 3rd arg to <<<b, t, shmem>>>
   extern __shared__ float sdata[];
 
@@ -57,69 +47,121 @@ __global__ void sharedMem_reduce_kernel(float *d_out, const float *d_in) {
 
 void reduce(float *d_out, float *d_intermediate, float *d_in, int size,
             bool usesSharedMemory) {
-
-  // assume that size is not greater than maxThreadsPerBlock^2
+  // assumes that size is not greater than maxThreadsPerBlock^2
   // and that size is a multiple of maxThreadsPerBlock
-  const int maxThreadPerBlock = 1024;
-
-  int threads = maxThreadPerBlock;
-  int blocks = size / maxThreadPerBlock;
-
+  const int maxThreadsPerBlock = 1024;
+  int threads = maxThreadsPerBlock;
+  int blocks = size / maxThreadsPerBlock;
   if (usesSharedMemory) {
-    sharedMem_reduce_kernel<<<blocks, threads, threads * sizeof(float)>>>(
+    shmem_reduce_kernel<<<blocks, threads, threads * sizeof(float)>>>(
         d_intermediate, d_in);
   } else {
-    globalMem_reduce_kernel<<<blocks, threads>>>(d_intermediate, d_in);
+    global_reduce_kernel<<<blocks, threads>>>(d_intermediate, d_in);
   }
-
   // now we're down to one block left, so reduce it
   threads = blocks; // launch one thread for each block in prev step
   blocks = 1;
-
   if (usesSharedMemory) {
-    sharedMem_reduce_kernel<<<blocks, threads, threads * sizeof(float)>>>(
-        d_intermediate, d_in);
+    shmem_reduce_kernel<<<blocks, threads, threads * sizeof(float)>>>(
+        d_out, d_intermediate);
   } else {
-
-    globalMem_reduce_kernel<<<blocks, threads>>>(d_out, d_intermediate);
+    global_reduce_kernel<<<blocks, threads>>>(d_out, d_intermediate);
   }
 }
 
-int main() {
-  const int ARRAYSIZE = 1 << 20;
-  const int ARRAYBYTES = ARRAYSIZE * sizeof(float);
-
-  // allocate host arrays
-  float *h_in = (float *)std::malloc(ARRAYBYTES);
-  for (int i = 0; i < ARRAYSIZE; ++i) {
-    h_in[i] = i;
+int main(int argc, char **argv) {
+  int deviceCount;
+  cudaGetDeviceCount(&deviceCount);
+  if (deviceCount == 0) {
+    fprintf(stderr, "error: no devices supporting CUDA.\n");
+    exit(EXIT_FAILURE);
   }
-  float h_out;
+  int dev = 0;
+  cudaSetDevice(dev);
 
-  // run reduce (serial) in host
-  reduce(&h_out, h_in, ARRAYSIZE);
-  printf("reduce calculated on host: %f\n", h_out);
+  cudaDeviceProp devProps;
+  if (cudaGetDeviceProperties(&devProps, dev) == 0) {
+    printf("Using device %d:\n", dev);
+    printf("%s; global mem: %dB; compute v%d.%d; clock: %d kHz\n",
+           devProps.name, (int)devProps.totalGlobalMem, (int)devProps.major,
+           (int)devProps.minor, (int)devProps.clockRate);
+  }
 
-  // reset
-  h_out = 0;
-  // declare gpu memo pointers
-  float *d_in, *d_out, *d_intermediate;
-  cudaMalloc((void **)&d_in, ARRAYBYTES);
-  cudaMalloc((void **)&d_intermediate, ARRAYBYTES);
+  const int ARRAY_SIZE = 1 << 20;
+  const int ARRAY_BYTES = ARRAY_SIZE * sizeof(float);
 
+  // generate the input array on the host
+  float h_in[ARRAY_SIZE];
+  float sum = 0.0f;
+  for (int i = 0; i < ARRAY_SIZE; i++) {
+    // generate random float in [-1.0f, 1.0f]
+    h_in[i] = -1.0f + (float)random() / ((float)RAND_MAX / 2.0f);
+    sum += h_in[i];
+  }
+
+  // declare GPU memory pointers
+  float *d_in, *d_intermediate, *d_out;
+
+  // allocate GPU memory
+  cudaMalloc((void **)&d_in, ARRAY_BYTES);
+  cudaMalloc((void **)&d_intermediate, ARRAY_BYTES); // overallocated
   cudaMalloc((void **)&d_out, sizeof(float));
 
-  // copy data to gpu;
-  cudaMemcpy(d_in, h_in, ARRAYSIZE, cudaMemcpyHostToDevice);
+  // transfer the input array to the GPU
+  cudaMemcpy(d_in, h_in, ARRAY_BYTES, cudaMemcpyHostToDevice);
 
-  // reduce
-  reduce(d_out, d_intermediate, d_in, ARRAYSIZE, false);
+  int whichKernel = 0;
+  if (argc == 2) {
+    whichKernel = atoi(argv[1]);
+  }
 
-  // copy back to host
+  cudaEvent_t start, stop;
+  cudaEventCreate(&start);
+  cudaEventCreate(&stop);
+  // launch the kernel
+  switch (whichKernel) {
+  case 0:
+    printf("Running global reduce\n");
+    cudaEventRecord(start, 0);
+    for (int i = 0; i < 1; i++) {
+      reduce(d_out, d_intermediate, d_in, ARRAY_SIZE, false);
+    }
+    cudaEventRecord(stop, 0);
+    break;
+  case 1:
+    printf("Running reduce with shared mem\n");
+    cudaEventRecord(start, 0);
+    for (int i = 0; i < 100; i++) {
+      reduce(d_out, d_intermediate, d_in, ARRAY_SIZE, true);
+    }
+    cudaEventRecord(stop, 0);
+    break;
+  default:
+    fprintf(stderr, "error: ran no kernel\n");
+    exit(EXIT_FAILURE);
+  }
+  cudaEventSynchronize(stop);
+  float elapsedTime;
+  cudaEventElapsedTime(&elapsedTime, start, stop);
+  elapsedTime /= 100.0f; // 100 trials
+
+  // copy back the sum from GPU
+  float h_out;
   cudaMemcpy(&h_out, d_out, sizeof(float), cudaMemcpyDeviceToHost);
-  printf("reduce calculated on Gpu: %f\n", h_out);
 
+  printf("average time elapsed: %f\n", elapsedTime);
+
+  float tolerance = 1e-3f;
+  if (fabs(sum - h_out) < tolerance) {
+    printf("Results match within tolerance (%.6f)\n", tolerance);
+  } else {
+    printf("Results differ! CPU: %f, GPU: %f\n", sum, h_out);
+  }
+
+  // free GPU memory allocation
   cudaFree(d_in);
-  cudaFree(d_out);
   cudaFree(d_intermediate);
+  cudaFree(d_out);
+
+  return 0;
 }
