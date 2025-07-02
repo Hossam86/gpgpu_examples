edcb41af00ddacb18d2c07c96701dc81010e4a30
Hossam Ali <hossamsamir@outlook.com>
2025-07-02 17:50:14 +0300

    problem set1 solution

--- 01_introduction/problem_set_1/reference_calc.h
+++ 01_introduction/problem_set_1/reference_calc.h
@@ -1,9 +1,9 @@
 #ifndef REFERENCE_H__
 #define REFERENCE_H__
+#include <cuda_runtime.h>
 
-void referenceCalculation(const uchar4* const rgbaImage,
-                          unsigned char *const greyImage,
-                          size_t numRows,
+void referenceCalculation(const uchar4 *const rgbaImage,
+                          unsigned char *const greyImage, size_t numRows,
                           size_t numCols);
 
 #endif
